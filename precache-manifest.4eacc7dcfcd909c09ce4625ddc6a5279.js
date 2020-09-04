15926f4a6b24a8cdb8d94347eb3a25fffdd0f692
Jean-Tarek Rishmawi <jtrishmawi@karhoo.com>
2020-09-04 19:34:49 +0200

    Updates

---
+++ precache-manifest.4eacc7dcfcd909c09ce4625ddc6a5279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e080fa6ebb76b4297f58bd7e7a5dc110",
+    "url": "/bev-subscriptions/index.html"
+  },
+  {
+    "revision": "2f275b4ec06cfe096ffb",
+    "url": "/bev-subscriptions/static/js/2.51031fa2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bev-subscriptions/static/js/2.51031fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b694bb78d9aa4b8aefd8",
+    "url": "/bev-subscriptions/static/js/main.fe9591f3.chunk.js"
+  },
+  {
+    "revision": "95154b831fc4e341d9d4",
+    "url": "/bev-subscriptions/static/js/runtime-main.34e75887.js"
+  }
+]);
