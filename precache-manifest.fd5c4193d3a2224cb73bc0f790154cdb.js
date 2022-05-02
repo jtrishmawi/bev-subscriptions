eaab742ff02c5a12acbb885e184e173fbb154c93
Tarek Rishmawi <jtrishmawi@gmail.com>
2022-05-03 00:10:50 +0200

    Updates

---
+++ precache-manifest.fd5c4193d3a2224cb73bc0f790154cdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bc3a06bb21f47f791514ff17bb16f5",
+    "url": "/bev-subscriptions/index.html"
+  },
+  {
+    "revision": "d3d31c139cdf90698b66",
+    "url": "/bev-subscriptions/static/js/2.72d06480.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/bev-subscriptions/static/js/2.72d06480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee048bb8bf4145c683c",
+    "url": "/bev-subscriptions/static/js/main.4a16b682.chunk.js"
+  },
+  {
+    "revision": "95154b831fc4e341d9d4",
+    "url": "/bev-subscriptions/static/js/runtime-main.34e75887.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bev-subscriptions/precache-manifest.b13929555da67d2aefb7218cd6cfe6ab.js"
+  "/bev-subscriptions/precache-manifest.fd5c4193d3a2224cb73bc0f790154cdb.js"
 );
 
 self.addEventListener('message', (event) => {
