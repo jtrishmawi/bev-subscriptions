0c4fb439f1378d329b0e8a9074da610f216f1eb1
Tarek Rishmawi <jtrishmawi@gmail.com>
2022-05-21 13:58:51 +0200

    safari fix

--- src/state/index.js
+++ src/state/index.js
@@ -41,34 +41,33 @@ export const withData = (WrappedComponent) => {
       (async () => {
         const request = await fetch(process.env.REACT_APP_API_URL);
         const response = await request.json();
-        const data = response.data
-          .map((submission) => {
-            const form_data = JSON.parse(submission.form_data);
-            form_data.nom = form_data.nom.trim();
-            if (form_data.joueurs) {
-              const joueurs = JSON.parse(form_data.joueurs);
-              form_data.joueurs = joueurs.sort((a, b) =>
-                a.capitaine < b.capitaine || a.vice_capitaine < b.vice_capitaine
-                  ? 1
-                  : -1
-              );
-            }
-            return {
-              ...submission,
-              form_data,
-            };
-          })
-          .sort((a, b) =>
-            a.form_data.nom.trim().charAt(0).toLowerCase() >
-            b.form_data.nom.trim().charAt(0).toLowerCase()
-              ? 1
-              : -1
-          )
-          .filter((item) =>
-            process.env.REACT_APP_SHOW_THIS_YEAR_ONLY === "true"
-              ? new Date(item.created_at) >= state.lastYear
-              : true
-          );
+        const data = [...response.data].map((submission) => {
+          const form_data = JSON.parse(submission.form_data);
+          form_data.nom = form_data.nom.trim();
+          if (form_data.joueurs) {
+            const joueurs = JSON.parse(form_data.joueurs);
+            form_data.joueurs = joueurs.sort((a, b) =>
+              a.capitaine < b.capitaine || a.vice_capitaine < b.vice_capitaine
+                ? 1
+                : -1
+            );
+          }
+          return {
+            ...submission,
+            form_data,
+          };
+        });
+        data.sort((a, b) =>
+          a.form_data.nom.trim().charAt(0).toLowerCase() >
+          b.form_data.nom.trim().charAt(0).toLowerCase()
+            ? 1
+            : -1
+        );
+        data.filter((item) =>
+          process.env.REACT_APP_SHOW_THIS_YEAR_ONLY === "true"
+            ? new Date(item.created_at) >= state.lastYear
+            : true
+        );
 
         let itemsPerGroup = [];
         groupes.forEach(
